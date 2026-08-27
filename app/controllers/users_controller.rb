321431f24f829caae62616bf562b0f9b8bc0b0ea
Tran Doan Huy <trandoanhuy@pixta-admins-MacBook-Pro.local>
2026-08-27 16:39:36 +0700

    use find_by instead of find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    unless @user
+      flash[:danger] = "User not found"
+      redirect_to root_path
+    end
   end
 
   def create
