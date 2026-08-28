70dbb030b914b871375eb1c5fdb3a8fdf1713ae3
Tran Doan Huy <trandoanhuy@pixta-admins-MacBook-Pro.local>
2026-08-28 10:25:42 +0700

    complete chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -16,4 +18,27 @@ class User < ApplicationRecord
             presence: true,
             length: { minimum: 6 }
   has_secure_password
+
+  def forget
+    update_column(:remember_digest, nil)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
